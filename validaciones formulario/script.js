854b6a1cc551e4948bc77ba00e8782d4261e82ee
lautaroeberle <louloufubol@gmail.com>
2023-11-24 11:51:18 -0300

    buena paja

---
+++ validaciones formulario/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function(event) {
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        
+        if (username.length < 4 || username.length > 10) {
+            alert("El nombre de usuario debe tener entre 4 y 10 caracteres.");
+            event.preventDefault(); //esto evita que se mande el form
+            return;
+        }
+
+       
+        if (password.length < 4) {
+            alert("La contraseña debe tener al menos 4 caracteres.");
+            event.preventDefault(); 
+            return;
+        }
+
+      
+    });
+});
